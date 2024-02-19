b5fd0ad4ccd57915c997b12e220737edf2ad3d31
Apple <apple@192.168.1.19>
2024-02-19 17:56:55 +0500

    final

--- server/index.js
+++ server/index.js
@@ -3,32 +3,25 @@ const  app = express();
 const http = require('http');
 const cors = require('cors')
 app.use(cors());
-app.use(express.json());
-
-// const { Server } = require('socket.io')
-// const server = http.createServer(app)
-
-// const io = new Server(server,{
-//     cors:{
-//         origin: "https://chat-jdry098bf-shehryars-projects-01ac2e84.vercel.app/",
-//         method:['GET',"POST"]
-//     }
-// })
-// io.on("connection",(socket)=>{
-//    console.log(`User Connect:${socket.id}`)
-
-//    socket.on('join_room',(data) =>{
-//     socket.join(data)
-//   })
-//   socket.on('send_message',(data) =>{
-//     console.log("Message received on server:", data); // Add this line to log
-//     socket.to(data.room).emit("received_message",data)
-//   })
-// });
-app.use('/testing', (req, res)=>{
-  res.send({data:"response is here"})
+const { Server } = require('socket.io')
+const server = http.createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        method:['GET',"POST"]
+    }
 })
+io.on("connection",(socket)=>{
+   console.log(`User Connect:${socket.id}`)
 
-// server.listen('https://chat-app-gcdi.vercel.app/',()=>{
-//     console.log(' Server is running')
-// })
+   socket.on('join_room',(data) =>{
+    socket.join(data)
+  })
+  socket.on('send_message',(data) =>{
+    console.log("Message received on server:", data); // Add this line to log
+    socket.to(data.room).emit("received_message",data)
+  })
+});
+server.listen(3001,()=>{
+    console.log(' Server is running')
+})
